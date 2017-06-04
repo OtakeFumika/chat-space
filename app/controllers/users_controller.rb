61c1d85c19f87472037598d57ed18603f7d469b6
pesu1234 <k.takafumi1110@gmail.com>
2017-06-04 20:07:27 +0900

    Create_user.edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :move_to_index
+
+  def edit
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to controller: :messages,action: :index
+    else
+      redirect_to controller: :messages,action: :edit
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to controller: :messages,action: :index
+    end
+  end
+end
