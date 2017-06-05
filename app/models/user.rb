5e131f315d6163ae5529a7d3af9d496b0dc73ef6
pesu1234 <k.takafumi1110@gmail.com>
2017-06-05 17:55:58 +0900

    Create models & controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
-  validates :name, presence: true, length: { minimum: 4}, length: { maximum: 20}, uniqueness: true
-  validates :email, presence: true, length: {minimum: 8}, length: { maximum: 50}, uniqueness: true
+  has_many :group_users
+  has_many :groups, through: :group_users
+  validates :name, presence: true, length: { minimum: 4, maximum: 20}, uniqueness: true
+  validates :email, presence: true, length: {minimum: 8, maximum: 50}, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
