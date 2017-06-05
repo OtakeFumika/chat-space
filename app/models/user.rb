a98ba6369509e1e8a454f4249df28aec10186e79
pesu1234 <k.takafumi1110@gmail.com>
2017-06-05 19:16:45 +0900

    Set up associations between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
+  has_many :group_users
+  has_many :groups, through: :group_users
   validates :name, presence: true, length: { minimum: 4, maximum: 20}, uniqueness: true
   validates :email, presence: true, length: {minimum: 8, maximum: 50}, uniqueness: true
   devise :database_authenticatable, :registerable,
