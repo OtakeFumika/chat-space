ef5c16b84c24fd07f8258719339d36af398c1afc
pesu1234 <k.takafumi1110@gmail.com>
2017-06-05 19:56:25 +0900

    Add new action

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,12 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  validates :name, presence: true
+
+  validate :error_message
+
+  def error_message
+    if name.empty?
+      errors.add(:name, "グループ名が空欄です")
+    end
 end
