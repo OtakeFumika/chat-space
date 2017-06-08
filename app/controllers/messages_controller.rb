2c5470378de923d438c42793cc743ba8e36af0bc
pesu1234 <k.takafumi1110@gmail.com>
2017-06-08 11:00:41 +0900

    Create message-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.new
+    if params[:group_id]
+      @group = Group.find(params[:group_id])
+    end
   end
 
   def create
+    @messages = Message.new
+    binding.pry
+    Message.create(text: message_params[:message], group_id: message_params[:group_id], user_id: current_user.id,image: message_params[:image])
+    redirect_to root_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message,:group_id,:image)
   end
 end
--- db/migrate/20170607041509_create_messages.rb
+++ db/migrate/20170607041509_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.text :text
       t.text :image
-      t.integer :use_id, foreign_key: true, index: true
+      t.integer :user_id, foreign_key: true, index: true
       t.integer :group_id, foreign_key: true, index: true
       t.timestamps
     end
