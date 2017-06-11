4c785d03ae4ed207ad8339481216dd3490e1889a
pesu1234 <k.takafumi1110@gmail.com>
2017-06-11 18:33:07 +0900

    Retouch view

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     it "is valid with a text" do
-        message = build(:message, image: "")
+        message = build(:message, image: nil)
         message.valid?
         expect(message).to be_valid
     end
 
     it "is valid with a image" do
-        message = build(:message, text: "")
+        message = build(:message, text: nil)
         message.valid?
         expect(message).to be_valid
     end
@@ -20,19 +20,19 @@ describe Message do
     end
 
     it "is invalid without a image & text" do
-        message = build(:message, text: "", image: "")
+        message = build(:message, text: nil, image: nil)
         message.valid?
-        expect(message.errors[:text]).to include("を入力してください")
+        expect(message.errors[:image]).to include("を入力してください")
     end
 
     it "is invalid without a group_id" do
-        message = build(:message, group_id: "")
+        message = build(:message, group_id: nil)
         message.valid?
         expect(message.errors[:group_id]).to include("を入力してください")
     end
 
     it "is invalid without a user_id" do
-        message = build(:message, user_id: "")
+        message = build(:message, user_id: nil)
         message.valid?
         expect(message.errors[:user_id]).to include("を入力してください")
     end
