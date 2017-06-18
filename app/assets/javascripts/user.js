079a822dd62df21b47eecd716a2c85ef67f95339
pesu1234 <k.takafumi1110@gmail.com>
2017-06-18 09:59:13 +0900

    Replace "

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,18 +1,18 @@
 $(function() {
   var preWord;
   function buildHTML(user){
-    var div = $('<div>', {"class": "chat-group-user clearfix", "id": "chat-group-add"});
-    var users = $('<p>', {"class": "chat-group-user__name"}).append(user.name);
-    var btn = $('<a>', {'class': 'user-search-add chat-group-user__btn chat-group-user__btn--add', 'data-user-id': user.id, 'data-user-name': user.name}).append("追加");
+    var div = $('<div>', {'class': 'chat-group-user clearfix', 'id': 'chat-group-add'});
+    var users = $('<p>', {'class': 'chat-group-user__name'}).append(user.name);
+    var btn = $('<a>', {'class': 'user-search-add chat-group-user__btn chat-group-user__btn--add', 'data-user-id': user.id, 'data-user-name': user.name}).append('追加');
     var result = div.append(users).append(btn);
     $('#user-search-result').append(result);
   }
 
   function buildClass(user){
-    var div = $('<div>', {"class": "chat-group-user clearfix js-chat-member", "id": "chat-group-user-8"});
-    var input = $('<input>').attr({name: "group[user_ids][]", type: "hidden", value: $(user).data('user-id')});
-    var name = $('<p>', {"class": "chat-group-user__name"}).text($(user).data('user-name'));
-    var btn = $('<a>', {"class": "user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn"}).append("削除");
+    var div = $('<div>', {'class': 'chat-group-user clearfix js-chat-member', 'id': 'chat-group-user-8'});
+    var input = $('<input>').attr({name: 'group[user_ids][]', type: 'hidden', value: $(user).data('user-id')});
+    var name = $('<p>', {'class': 'chat-group-user__name'}).text($(user).data('user-name'));
+    var btn = $('<a>', {'class': 'user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'}).append('削除');
 
     var result = div.append(input).append(name).append(btn);
     $('#chat-group-users').append(result);
@@ -22,12 +22,12 @@ $(function() {
     e.preventDefault()
 
     var $input = $('#user-search-field').val();
-    var $search = $input.split(" ").filter(function(e) {return e;});
+    var $search = $input.split(' ').filter(function(e) {return e;});
 
     $.ajax({
       type: 'GET',
       url: '/users',
-      data: ("name=" + $search),
+      data: ('name=' + $search),
       dataType: 'json',
       contentType: false,
       processData: false
@@ -44,7 +44,7 @@ $(function() {
       preWord = data;
     })
     .fail(function(data) {
-      alert("検索に失敗しました");
+      alert('検索に失敗しました');
     });
   });
   $('#user-search-result').on('click', '.user-search-add', function(){
